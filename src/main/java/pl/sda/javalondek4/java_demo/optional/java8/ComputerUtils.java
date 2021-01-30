0f12065bc9441f252e995ae08b858124454628be
Aleksandra Szafruga <ola.szafruga@gmail.com>
2021-01-30 11:51:21 +0100

    [change] - using flatMap with Optional

--- src/main/java/pl/sda/javalondek4/java_demo/optional/java8/ComputerUtils.java
+++ src/main/java/pl/sda/javalondek4/java_demo/optional/java8/ComputerUtils.java
@@ -1,5 +1,7 @@
 package pl.sda.javalondek4.java_demo.optional.java8;
 
+import java.util.Optional;
+
 public class ComputerUtils {
 
     private ComputerUtils() {
@@ -7,6 +9,22 @@ public class ComputerUtils {
     }
 
     public static String getGraphicCardModelGoodApproach(Computer computer) {
+
+        Optional.ofNullable(computer)
+
+                //.map(comp -> comp.getGraphicCard().get()) very dangerous!
+                //.map(comp -> comp.getGraphicCard().orElse(null)) can be ok, but
+                .flatMap(comp -> comp.getGraphicCard()) //if Optional inside use flatMap
+                .flatMap(gc -> gc.getModel())
+                .orElse("no model");
+
+        var maybeGraphicCard = Optional.ofNullable(computer)
+                .map(comp -> comp.getGraphicCard())
+                .orElse(Optional.empty());
+
+        maybeGraphicCard.map(gc -> gc.getModel())
+                .orElse(Optional.empty())
+                .orElse("no model");
 
         return null;
 
